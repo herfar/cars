49151dcbd3383219f99dd95f79d46cd05b85a9a1
herfar <94548763+herfar@users.noreply.github.com>
2022-11-11 18:36:53 +0300

    Update CarController.java

--- httpCars/src/main/java/web/controller/CarController.java
+++ httpCars/src/main/java/web/controller/CarController.java
@@ -5,20 +5,21 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.ImpServiceForCar;
 
 @Controller
 @RequestMapping("/cars")
 public class CarController {
-    private ImpServiceForCar impServiceForCar;
-
+    
+    private final serviceForCar serviceForCar;
+    
     @Autowired
-    public CarController(ImpServiceForCar impServiceForCar) {
-        this.impServiceForCar = impServiceForCar;
+    public CarController(serviceForCar serviceForCar) {
+        this.serviceForCar = serviceForCar;
     }
+
     @GetMapping()
     public String countCars(@RequestParam(name = "count", defaultValue = "5") int count, ModelMap model) {
-        model.addAttribute("cars", impServiceForCar.countCars(count));
+        model.addAttribute("cars", serviceForCar.countCars(count));
         return "cars";
     }
 }
