b4e551de3e05526056feaf08794882172aeaecba
herfar <94548763+herfar@users.noreply.github.com>
2022-11-11 18:33:15 +0300

    Update ImpServiceForCar.java

--- httpCars/src/main/java/web/service/ImpServiceForCar.java
+++ httpCars/src/main/java/web/service/ImpServiceForCar.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Service
 public class ImpServiceForCar implements serviceForCar {
 
     private List<Car> cars;
@@ -36,36 +37,3 @@ public class ImpServiceForCar implements serviceForCar {
         return carsFromCountCars;
     }
 }
-
-
-
-
-
-
-
-
-
-
-////    private List<Car> cars;
-////    {
-////        cars = new ArrayList<>();
-////
-////        cars.add(new Car("volvo", 5, 12));
-////        cars.add(new Car("porshe", 3, 10));
-////        cars.add(new Car("bmw", 4, 8));
-////        cars.add(new Car("toyota", 8, 14));
-////        cars.add(new Car("kia", 2, 6));
-////    }
-////    @Override
-////    public List<Car> countCars(List<Car> cars, int size) {
-////
-////        List<Car> carsFromCountCars = new ArrayList<>();
-////        if (size >= cars.size()) {
-////            return cars;
-////        } else {
-////            for (int y = 0; y <= size-1; y++) {
-////                carsFromCountCars.add(cars.get(y));
-////            }
-////        }
-////        return carsFromCountCars;
-////    }
